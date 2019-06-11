653f54d076223ed5334827c533ef20964d689154
Kang0 <kevinkangg@gmail.com>
2019-06-11 19:11:34 -0400

    create new Challenge component DisplayChallenges

--- src/containers/ChallengeContainer.js
+++ src/containers/ChallengeContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 //import challenge components
 import CreateChallenge from '../components/challenges/CreateChallenge'
+import DisplayChallenges from '.../components/challenges/DisplayChallenges'
 //import challenge actions
 import { postChallenge } from '../actions/challenges/actionChallenge'
 
@@ -11,6 +12,7 @@ class ChallengeContainer extends Component {
         return (
             <div>
                 <CreateChallenge postChallenge={this.props.postChallenge}/>
+                <DisplayChallenges />
             </div>
         )
     }
