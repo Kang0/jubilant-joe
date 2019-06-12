931d214a41b18e81a16643b7a77c8cedf95e499d
Kang0 <kevinkangg@gmail.com>
2019-06-12 00:47:39 -0400

    render UserPage if user is not null or null

--- src/components/user/UserPage.js
+++ src/components/user/UserPage.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react'
 const baseUrl = 'http://localhost:3001'
 
 class UserPage extends Component {
+    state = {
+        user: null
+    }
 
+    render() {
+        return (
+            <div>
+                {this.state.user ? (
+                    <div>
+                        <h1>{this.state.user.name}</h1>
+                        <p>{this.state.user.email}</p>
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
+        )
+    }
 }
 
 export default UserPage
