e27d5d3c74f2ee50fec1d48f7d548d0869bfbd20
Kang0 <kevinkangg@gmail.com>
2019-06-16 14:30:20 -0400

    DisplayChallenges shows ChallengeCard pres comp inside Accordion

--- src/components/challenges/ChallengeCard.js
+++ src/components/challenges/ChallengeCard.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import { Card, Button } from 'semantic-ui-react';
 
-const ChallengeCard = (props) => {
-    debugger;
+const ChallengeCard = ({ handleCardClick, challenge, handleOnButtonClick }) => {
 
     return(
-        <Card onClick={event => props.handleCardClick(event)}>
+        <Card onClick={event => handleCardClick(event)}>
             <Card.Content>
-                <Card.Header>{props.challenge.name}</Card.Header>
-                <Card.Meta>You have {props.challenge.daysLeft} days to go!</Card.Meta>
+                <Card.Header>{challenge.name}</Card.Header>
+                <Card.Meta>You have {challenge.daysLeft} days to go!</Card.Meta>
                 <Card.Description>
                     This is placeholder text until I can update this.
                 </Card.Description>
             </Card.Content>
             <Card.Content extra>
                 {
-                    props.challenge.clicked ? 
+                    challenge.clicked ? 
                         <Button basic color="red" disabled>Already completed for the day</Button>
                         :
-                        <Button basic color="green" id={props.challenge.id} onClick={e=>this.handleOnClick(e)}>I completed this today</Button>                               
+                        <Button basic color="green" id={challenge.id} onClick={e=>handleOnButtonClick(e)}>I completed this today</Button>                               
                 }
             </Card.Content>
         </Card>
--- src/components/challenges/DisplayChallenges.js
+++ src/components/challenges/DisplayChallenges.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
 import ChallengeCard from './ChallengeCard'
-import { Card } from 'semantic-ui-react'
+import { Card, Accordion } from 'semantic-ui-react'
 
 class DisplayChallenges extends Component {
 
-    handleOnClick = event => {
+    state = { activeIndex: 0 } //keeping track of which accordion is open
+
+    handleOnButtonClick = event => {
         this.props.buttonClick(event.target.id)
         //passing in the challenge id as an argument
         this.props.clickAddTwoCurrency()
     }
 
-    handleCardClick = event => {
+    handleCardClick = (event, titleProps) => {
+        const { index } = titleProps
+        const { activeIndex } = this.state
+        const newIndex = activeIndex === index ? -1 : index
+
         debugger;
+
+        this.setState({ activeIndex: newIndex })
     }
     
     render() {
+
+        const { activeIndex } = this.state
         
-        const renderChallenge = this.props.challenges.map(challenge => <ChallengeCard challenge={challenge} handleCardClick={this.handleCardClick} handleOnClick={this.handleOnClick} />)
+        const renderChallenge = this.props.challenges.map(challenge => {
+            return(
+                <React.Fragment>
+                    <Accordion.Title active={activeIndex === challenge.id} index={challenge.id} onClick={this.handleCardClick}>
+                        <ChallengeCard challenge={challenge} handleCardClick={this.handleCardClick} handleOnButtonClick={this.handleOnButtonClick} />
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === challenge.id}>
+                        <p>
+                            This content will hopefully be filled with a working calendar object and some more information about the challenge.
+                        </p>
+                    </Accordion.Content>
+                </React.Fragment>
+            )
+        })
 
         return(
             <div>
-                <Card.Group>
+                <Accordion fluid styled>
                     {renderChallenge}
-                </Card.Group>
+                </Accordion>
             </div>
         )
     }
