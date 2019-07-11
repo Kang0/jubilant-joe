9a6c1a145805a910729a4029af7f9e1be38f89ab
Kang0 <kevinkangg@gmail.com>
2019-07-11 13:55:45 -0400

    clicking date on calendar now changes what is shown in the challenge media contianer

--- src/containers/ChallengeMediaContainer.js
+++ src/containers/ChallengeMediaContainer.js
@@ -3,17 +3,32 @@ import { connect } from 'react-redux';
 
 class ChallengeMediaContainer extends Component {
     render(){
-        debugger
-        return(
-            <p>hi</p>
-        )
+        let { calendarDate, id, challengeDate } = this.props
+        //returns ["YYYY", "M", "D"]
+        let calendarDateArray = calendarDate.split("-")
+        if(challengeDate.length > 0) {
+            let month = calendarDateArray[1]
+            let day = calendarDateArray[2]
+            let selectedDate = challengeDate[0][id].find(challenge => challenge["months"] == month && challenge["date"] == day)
+            return(
+                <div>
+                    <h3>Month {selectedDate["months"]}</h3>
+                    <h4>Day {selectedDate["date"]}</h4>
+                    <h4>Year {selectedDate["years"]}</h4>
+                </div>
+            )
+        } else {
+            return(
+                <p>Not Loaded</p>
+            )
+        }
     }
 }
 
 const mapStateToProps = (state, ownProps) => {
     let { id } = ownProps
-    return{
-        selectedDate: state.calendar.find(challenge => challenge[id])
+    return {
+        challengeDate: state.calendar.filter(challenge => challenge[id])
     }
 }
 
